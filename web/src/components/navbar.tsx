bd19a4e5f0c151232f439736d6245ac1b5d41dcf
atzaytsev <tonyzaitsman@gmail.com>
2025-02-09 00:22:42 +0500

    fix build type errors

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -9,10 +9,10 @@ import {
   NavbarItem,
   NavbarMenuToggle,
   NavbarMenu,
-  NavbarMenuItem,
+  // NavbarMenuItem,
 } from "@heroui/navbar";
-import { link as linkStyles } from "@heroui/theme";
-import clsx from "clsx";
+// import { link as linkStyles } from "@heroui/theme";
+// import clsx from "clsx";
 
 import { siteConfig } from "@/config/site.ts";
 import { ThemeSwitch } from "@/components/theme-switch.tsx";
@@ -61,20 +61,20 @@ export const Navbar = () => {
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <Link
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </Link>
-            </NavbarItem>
-          ))}
+          {/*{siteConfig.navItems.map((item) => (*/}
+          {/*  <NavbarItem key={item.href}>*/}
+          {/*    <Link*/}
+          {/*      className={clsx(*/}
+          {/*        linkStyles({ color: "foreground" }),*/}
+          {/*        "data-[active=true]:text-primary data-[active=true]:font-medium"*/}
+          {/*      )}*/}
+          {/*      color="foreground"*/}
+          {/*      href={item.href}*/}
+          {/*    >*/}
+          {/*      {item.label}*/}
+          {/*    </Link>*/}
+          {/*  </NavbarItem>*/}
+          {/*))}*/}
         </div>
       </NavbarContent>
 
@@ -89,10 +89,10 @@ export const Navbar = () => {
           {/*<Link isExternal href={siteConfig.links.discord} title="Discord">*/}
           {/*  <DiscordIcon className="text-default-500" />*/}
           {/*</Link>*/}
-          {/*<Link isExternal href={siteConfig.links.github} title="GitHub">*/}
-          {/*  <GithubIcon className="text-default-500" />*/}
-          {/*</Link>*/}
-          <ThemeSwitch />
+          <Link isExternal href={siteConfig.links.github} title="GitHub">
+            <GithubIcon className="text-default-500" />
+          </Link>
+          {/*<ThemeSwitch />*/}
         </NavbarItem>
         {/*<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>*/}
         {/*<NavbarItem className="hidden md:flex">*/}
@@ -120,23 +120,23 @@ export const Navbar = () => {
       <NavbarMenu>
         {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
+          {/*{siteConfig.navMenuItems.map((item, index) => (*/}
+          {/*  <NavbarMenuItem key={`${item}-${index}`}>*/}
+          {/*    <Link*/}
+          {/*      color={*/}
+          {/*        index === 2*/}
+          {/*          ? "primary"*/}
+          {/*          : index === siteConfig.navMenuItems.length - 1*/}
+          {/*            ? "danger"*/}
+          {/*            : "foreground"*/}
+          {/*      }*/}
+          {/*      href="#"*/}
+          {/*      size="lg"*/}
+          {/*    >*/}
+          {/*      {item.label}*/}
+          {/*    </Link>*/}
+          {/*  </NavbarMenuItem>*/}
+          {/*))}*/}
         </div>
       </NavbarMenu>
     </HeroUINavbar>
